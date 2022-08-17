a832215d75e752b8054c38c57f8cc10f64656a54
Stephane Carrez <Stephane.Carrez@gmail.com>
2022-08-17 23:37:51 +0200

    Break build

--- src/gollum-main.adb
+++ src/gollum-main.adb
@@ -2,5 +2,5 @@ with Ada.Text_IO;
 with Ada.Directories;
 procedure Gollum.Main is
 begin
-   Ada.Text_IO.Put_Line ("gollum fixed again");
+   Ada.Text_IO.Put_Line ("gollum fixed again" + bug);
 end Gollum.Main;
