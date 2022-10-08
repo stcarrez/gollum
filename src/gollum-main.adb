6d3384111959bfef7d4851240e13da9d6ba28faf
Stephane Carrez <Stephane.Carrez@gmail.com>
2022-10-08 17:06:43 +0200

    Repair build

--- src/gollum-main.adb
+++ src/gollum-main.adb
@@ -2,5 +2,5 @@ with Ada.Text_IO;
 with Ada.Directories;
 procedure Gollum.Main is
 begin
-   Ada.Text_IO.Put_Line ("gollum fixed again" & bug);
+   Ada.Text_IO.Put_Line ("gollum fixed again");
 end Gollum.Main;
